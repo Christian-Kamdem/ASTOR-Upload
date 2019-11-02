1e9af136824202bbba97519046ad3e75f58e73cf
Christian Kamdem <christiankamdem2@gmail.com>
2019-11-02 18:38:16 +0100

    First comit

---
+++ entryPoint.php
@@ -0,0 +1,21 @@
+<?php
+//header('Access-Control-Allow-Origin: *');
+//declare(strict_types=1);
+//require_once 'Modules/domainOrigin.php';
+if(1/*domainOrigin() === true*/){
+	//header('Access-Control-Allow-Origin:'.$_SERVER['HTTP_ORIGIN']);
+	$dataReceive = json_decode(file_get_contents('php://input'));
+	$data = $dataReceive->data;
+	$requestName = strip_tags($dataReceive->requestName);
+	$allowedModules = ['uploadImage'];
+	//Switching following the type of the request
+	if(in_array($requestName,$allowedModules,true) === true){
+		include "modules/{$requestName}.php";
+		echo $requestName($data);
+	}else{
+		echo json_encode(array('message' => 'Request name not found!','error' => true));
+	}
+}else {
+	echo json_encode(array('message' => 'Domain origin not allowed!','error' => false));
+}
+?>
